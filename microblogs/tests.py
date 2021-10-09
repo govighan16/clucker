37053c337cd5f6ef6fdd31e7f9a1426d12ef8882
Govighan Krishnakumar <govighan.krishnakumar@kcl.ac.uk>
2021-10-09 19:58:15 +0100

    Enforce input validation on User model

--- microblogs/tests.py
+++ microblogs/tests.py
@@ -1,3 +1,141 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
+from .models import User
 
-# Create your tests here.
+
+class UserModelTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            '@johndoe',
+            first_name = 'John',
+            last_name = 'Doe',
+            email = 'johndoe@example.org',
+            password = 'Password123',
+            bio = 'The quick brown fox jumps over the lazy dog'
+        )
+
+
+    def test_valid_user(self):
+        self._assert_user_is_valid()
+
+
+    def test_username_cannot_be_blank(self):
+         self.user.username = ''
+         self._assert_user_is_invalid()
+
+    def test_username_can_be_30_characters_long(self):
+        self.user.username = '@' + 'x' * 29
+        self._assert_user_is_valid()
+
+    def test_username_cannot_be_over_30_characters_long(self):
+        self.user.username = '@' + 'x' * 30
+        self._assert_user_is_invalid()
+
+    def test_username_must_be_unique(self):
+        second_user = self._create_second_user()
+        self.user.username = second_user.username
+        self._assert_user_is_invalid()
+
+    def test_username_must_start_with_at_symbol(self):
+        self.user.username = 'johndoe'
+        self._assert_user_is_invalid()
+
+    def test_username_must_contain_only_alphanumbericals_after_at(self):
+        self.user.username = '@john!doe'
+        self._assert_user_is_invalid()
+
+    def test_username_must_contain_at_least_3_alphanumbericals_after_at(self):
+        self.user.username = '@jo'
+        self._assert_user_is_invalid()
+
+    def test_username_may_contain_numbers(self):
+        self.user.username = '@john123'
+        self._assert_user_is_valid()
+
+    def test_username_must_contain_only_one_at(self):
+        self.user.username = '@@johndoe'
+        self._assert_user_is_invalid()
+
+    def test_firstname_must_not_be_blank(self):
+        self.user.first_name = ''
+        self._assert_user_is_invalid()
+
+    def test_firstname_may_already_exist(self):
+        second_user = self._create_second_user()
+        self.user.first_name = second_user.first_name
+        self._assert_user_is_valid()
+
+    def test_firstname_has_a_maximum_of_50_characters(self):
+        self.user.first_name = 'x' * 51
+        self._assert_user_is_invalid()
+
+    def test_lastname_must_not_be_blank(self):
+        self.user.last_name = ''
+        self._assert_user_is_invalid()
+
+    def test_lastname_may_already_exist(self):
+        second_user = self._create_second_user()
+        self.user.last_name = second_user.last_name
+        self._assert_user_is_valid()
+
+    def test_lastname_has_a_maximum_of_50_characters(self):
+        self.user.last_name = 'x' * 51
+        self._assert_user_is_invalid()
+
+    def test_email_must_be_unique(self):
+        self.user.email = 'janedoe@example.org'
+        second_user = self._create_second_user()
+        self._assert_user_is_invalid()
+
+    def test_email_is_appropriate(self):
+        self.user.email = 'lolol@@kcl..org'
+        self._assert_user_is_invalid()
+
+    def test_email_must_contain_username(self):
+        self.user.email = '@kcl.org'
+        self._assert_user_is_invalid()
+
+    def test_email_must_contain_domain_name(self):
+        self.user.email = 'johndoe@.org'
+        self._assert_user_is_invalid()
+
+    def test_bio_may_be_blank(self):
+        self.user.bio = ''
+        self._assert_user_is_valid()
+
+    def test_bio_may_exist(self):
+        second_user = self._create_second_user()
+        self.user.bio = second_user.bio
+        self._assert_user_is_valid()
+
+    def test_bio_must_not_contain_more_than_520_characters(self):
+        self.user.bio = 'x' * 521
+        self._assert_user_is_invalid()
+
+    def test_bio_may_contain_520_characters(self):
+        self.user.bio = 'x' * 520
+        self._assert_user_is_valid()
+
+
+
+    def _assert_user_is_valid(self):
+        try:
+            self.user.full_clean()
+        except (ValidationError):
+            self.fail('Test user should fail')
+
+    def _assert_user_is_invalid(self):
+        with self.assertRaises(ValidationError):
+            self.user.full_clean()
+
+    def _create_second_user(self):
+        user = User.objects.create_user(
+            '@janedoe',
+            first_name = 'Jane',
+            last_name = 'Doe',
+            email = 'janedoe@example.org',
+            password = 'Password123',
+            bio = "This is Jane's profile"
+        )
+        return user
