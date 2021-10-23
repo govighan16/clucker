6a875a816b8f65a5e32cb86e12fbe282766ec6ee
Govighan Krishnakumar <govighan.krishnakumar@kcl.ac.uk>
2021-10-24 00:40:59 +0100

    Fix exercise 2.2 with 100 users in seed

--- microblogs/management/commands/seed.py
+++ microblogs/management/commands/seed.py
@@ -27,7 +27,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         #print("WARNING: The seed command has not yet been implemented yet.")
         #print('@' + self.faker.user_name())
-        for i in range(20):
+        for i in range(100):
         #user = User.objects.create_user(self.fake.first_name(), self.fake.last_name(), self.fake.email(),self.faker.password(), self.fake.text())
             user = User.objects.create_user(username = '@' + self.faker.user_name(), first_name = self.faker.first_name(), last_name = self.faker.last_name(), email = self.faker.email(), password = self.faker.password(), bio = self.faker.text())
             user.save()
