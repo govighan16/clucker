ab709bc0cca6ab46f15478ba0ca8ffb1ba344d87
Govighan Krishnakumar <govighan.krishnakumar@kcl.ac.uk>
2021-10-25 16:41:14 +0100

    Add LogInForm with tests

--- microblogs/forms.py
+++ microblogs/forms.py
@@ -6,6 +6,11 @@ from django.contrib.auth import get_user_model #
 
 User = get_user_model() #
 
+class LogInForm(forms.Form):
+    username = forms.CharField(label = 'Username')
+    password = forms.CharField(label = 'Password', widget = forms.PasswordInput())
+
+
 class SignUpForm(forms.ModelForm):
     class Meta:
         model = User
